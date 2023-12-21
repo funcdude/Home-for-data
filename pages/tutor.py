80165a9bb2f0e8d53bc93f74edea89dbac47e833
funcdude <131005085+funcdude@users.noreply.github.com>
2023-12-21 09:03:17 -0500

    Create tutor.py
    Connecting with Tutor Quizzer , OpenAI assistant

---
+++ pages/tutor.py
@@ -0,0 +1,147 @@
+import streamlit as st
+import langchain
+import os
+
+from langchain import OpenAI
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import FAISS 
+from langchain.text_splitter import CharacterTextSplitter
+from PyPDF2 import PdfReader
+from langchain.chains.question_answering import load_qa_chain
+
+load_dotenv()  # take environment variables from .env
+
+OpenAI.api_key = os.getenv("OPENAI_API_KEY")
+
+QUIZ_TUTOR_ID = "asst_Qjw6vQ6WRuIwyyfMtTPYIZJo"
+
+st.title(' Quizzes based on an uploaded PDF document')
+
+with st.sidebar:
+  openai_api_key = st.text_input('OpenAI API Key')
+
+  # Try to check if API key is empty
+
+  try:
+    if not openai_api_key:
+        raise ValueError("The API key is empty")
+    
+    # You can now use openai_api_key for further actions
+    
+  except ValueError as e:
+    # Displaying a message to the user to input the API key
+    st.warning(str(e))
+
+    # If API key is provided via input, set it as environment variable
+if openai_api_key:
+    os.environ['OPENAI_API_KEY'] = openai_api_key
+
+uploaded_file = st.file_uploader("Upload a PDF document", type="pdf")
+
+question = st.text_input(
+    "Quiz yourself about the file you uploaded",
+    placeholder="Can you create a quiz for me?",
+    disabled=not uploaded_file,
+)
+
+if uploaded_file and question and not openai_api_key:
+    st.info("Please add your OPEN AI API key to continue.")
+
+"""
+doc_reader = PdfReader(uploaded_file)
+# read data from the file and put them into a variable called raw_text
+raw_text = ''
+for i, page in enumerate(doc_reader.pages):
+    text = page.extract_text()
+    if text:
+        raw_text += text
+# st.info(len(raw_text))
+
+# Splitting up the text into smaller chunks for indexing
+text_splitter = CharacterTextSplitter(        
+    separator = "\n",
+    chunk_size = 1000,
+    chunk_overlap  = 200, #striding over the text
+    length_function = len,
+)
+pages = text_splitter.split_text(raw_text)
+
+# st.info(pages[1])
+
+# Download embeddings from OpenAI, setting up FAISS for similarity search
+
+embeddings = OpenAIEmbeddings()
+
+docsearch = FAISS.from_texts(pages, embeddings)
+
+chain = load_qa_chain(OpenAI(), chain_type="stuff") # we are going to stuff all the docs in at once
+docs = docsearch.similarity_search(question,k=10) # the k-value tells how many similar values to return
+results = chain({"input_documents": docs, "question": question}, return_only_outputs=True)
+                   
+chain.run(input_documents=docs, question=question)
+   
+st.write(results)
+"""
+
+#So that we can see the JSON outputs from the model
+import json
+
+def show_json(obj):
+    display(json.loads(obj.model_dump_json()))
+
+# using the OpenAI librarires, we create two functions 
+
+client = OpenAI()
+
+# functions to submit a message, containing assistant_id, thread_id - and get a response in the thread
+def submit_message(assistant_id, thread, user_message):
+    client.beta.threads.messages.create(
+        thread_id=thread.id, role="user", content=user_message
+    )
+    return client.beta.threads.runs.create(
+        thread_id=thread.id,
+        assistant_id=assistant_id,
+    )
+
+def get_response(thread):
+    return client.beta.threads.messages.list(thread_id=thread.id, order="asc")
+thread = client.beta.threads.create()
+
+
+#function that creates a thread and runs them with the message to the target assistant
+
+def create_thread_and_run(user_input):
+    thread = client.beta.threads.create()
+    run = submit_message(QUIZ_TUTOR_ID, thread, user_input)
+    return thread, run
+
+# Emulating concurrent user requests
+thread1, run1 = create_thread_and_run(
+    "Quiz me on the paper"
+)
+
+# Pretty printing helper
+def pretty_print(messages):
+    print("# Messages")
+    for m in messages:
+        print(f"{m.role}: {m.content[0].text.value}")
+    print()
+
+# Waiting in a loop
+def wait_on_run(run, thread):
+    while run.status == "queued" or run.status == "in_progress":
+        run = client.beta.threads.runs.retrieve(
+            thread_id=thread.id,
+            run_id=run.id,
+        )
+        time.sleep(0.5)
+    return run
+# Wait for Run 1
+run1 = wait_on_run(run1, thread1)
+pretty_print(get_response(thread1))
+
+# Wait for Run 2
+#run2 = wait_on_run(run2, thread2)
+#pretty_print(get_response(thread2))
+
+st.write(thread1)
